3e295b29d176dc86980716ab3456aa533f89e7fe
yying8 <yueyan.ying0256@gmail.com>
2025-06-09 14:48:17 -0400

    Time panel complete

--- Assets/Scripts/StartButtonController.cs
+++ Assets/Scripts/StartButtonController.cs
@@ -2,12 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ExploreButton : MonoBehaviour
+public class StartButtonController : MonoBehaviour
 {
     public GameObject timePanel;
     public void OnButtonClick()
     {
-        Debug.Log(" Debug: You click the Explore button");
-        timePanel.SetActive(true);
+        Debug.Log(" Debug: You click the start button");
+        timePanel.SetActive(false);
     }
 }
