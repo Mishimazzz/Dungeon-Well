ca9821f8d920aabd363f6144ff9c743bfcd78c10
yying8 <yueyan.ying0256@gmail.com>
2025-06-09 20:58:28 -0400

    Add Farm Scene

--- Assets/Scripts/SceneManager.cs
+++ Assets/Scripts/SceneManager.cs
@@ -1,18 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
+public enum SceneType { Well, Farm }
 public class SceneManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void LoadSceneByName(SceneType type)
     {
-        
+        switch (type)
+        {
+            case SceneType.Well:
+                UnityEngine.SceneManagement.SceneManager.LoadScene("WellScene");
+                break;
+            case SceneType.Farm:
+                UnityEngine.SceneManagement.SceneManager.LoadScene("FarmScene");
+                break;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    //switch scenes
+    public void LoadWell() => LoadSceneByName(SceneType.Well);
+    public void LoadFarm() => LoadSceneByName(SceneType.Farm);
+
+    public void LoadScene()
     {
-        
+        string currentSceneName = UnityEngine.SceneManagement.SceneManager.GetActiveScene().name;
+        // Debug.Log(currentSceneName);
+        if (currentSceneName.Equals("WellScene")) LoadFarm();
+        else if (currentSceneName.Equals("FarmScene")) LoadWell();
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Quit Game");
     }
 }
