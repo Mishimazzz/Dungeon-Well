0d774c83400152c057ffa4b0f74570382e8b016a
yying8 <yueyan.ying0256@gmail.com>
2025-06-19 17:13:17 +0800

    fix time display bug

--- Assets/Scripts/Time/TimeStopController.cs
+++ Assets/Scripts/Time/TimeStopController.cs
@@ -7,9 +7,13 @@ public class TimeStopController : MonoBehaviour
 {
     public GameObject timeStopPanel;
     public GameObject timeCountDownPanel;
+    public GameObject timeUpPanel;
     public TimeController timeController;
     public TimeCountDownController timeCountDownController;
     public TextMeshProUGUI exploreButtonText;
+    public ButtonManage buttonManage;
+    public ItemManager itemManager;
+    public TextMeshProUGUI executeHour; public TextMeshProUGUI executeMin; public TextMeshProUGUI executeSec;
 
     // hour, min, sec that left
     public int hourLeft; public int minLeft; public int secLeft;
@@ -17,7 +21,6 @@ public class TimeStopController : MonoBehaviour
     public void SelectYes()
     {
         //stop the time, and find hourLeft,min,sec
-        
         hourLeft = timeCountDownController.hourLeft; minLeft = timeCountDownController.minLeft; secLeft = timeCountDownController.secLeft;
         Debug.Log("hourLeft:" + hourLeft);
         Debug.Log("minLeft:" + minLeft);
@@ -27,6 +30,14 @@ public class TimeStopController : MonoBehaviour
         //whole system action
         timeCountDownPanel.SetActive(false);
         timeStopPanel.SetActive(false);
+        timeUpPanel.SetActive(true);
+
+        buttonManage.TotalFullExecuteTime = buttonManage.ComputeFullExecuteTime();
+        executeHour.text = buttonManage.executeHour.ToString("D2");
+        executeMin.text = buttonManage.executeMin.ToString("D2");
+        executeSec.text = buttonManage.executeSec.ToString("D2");
+        itemManager.SpawItem();
+
         exploreButtonText.text = "Explore";
         timeController.hour = 0; timeController.min = 0; timeController.sec = 0;
         RefreshTimeUI();
@@ -46,21 +57,35 @@ public class TimeStopController : MonoBehaviour
 
     public float ComputeExecuteTime()
     {
-        int executeHour;
-        if (timeCountDownController.tempHour > 0) executeHour = Mathf.Abs(timeCountDownController.tempHour - 1 - hourLeft);
-        else executeHour = Mathf.Abs(timeCountDownController.tempHour - hourLeft);
+        // 起始时间
+        int startHour = timeCountDownController.tempHour;
+        int startMin = timeCountDownController.tempMin;
+        int startSec = timeCountDownController.tempSec;
 
-        if (timeCountDownController.tempMin == 0) timeCountDownController.tempMin = 60;
-        if (timeCountDownController.tempSec == 0) timeCountDownController.tempSec = 60;
+        // 剩余时间
+        int leftHour = hourLeft;
+        int leftMin = minLeft;
+        int leftSec = secLeft;
 
-        int executeMin = Mathf.Abs(timeCountDownController.tempMin -1 - minLeft);
-        int executeSec = Mathf.Abs(timeCountDownController.tempSec - secLeft);
-        // Debug.Log("executeHour:" + executeHour);
-        // Debug.Log("executeMin:" + executeMin);
-        // Debug.Log("executeSec:" + executeSec);
-        float tempTotalExecuteTime = executeHour*60 + executeMin + executeSec / 60f;
-        // Debug.Log("TotalExecuteTime:" + TotalExecuteTime);
-        return tempTotalExecuteTime;
+        // 转为总秒数
+        int totalStartSec = startHour * 3600 + startMin * 60 + startSec;
+        int totalLeftSec = leftHour * 3600 + leftMin * 60 + leftSec;
+
+        // 实际消耗的秒数
+        int usedSec = totalStartSec - totalLeftSec;
+        if (usedSec < 0) usedSec = 0;
+
+        // 转回时分秒
+        int executeHour = usedSec / 3600;
+        int executeMin = (usedSec % 3600) / 60;
+        int executeSec = usedSec % 60;
+
+        buttonManage.executeHour = executeHour;
+        buttonManage.executeMin = executeMin;
+        buttonManage.executeSec = executeSec;
+
+        buttonManage.TotalFullExecuteTime = executeHour * 60 + executeMin + executeSec / 60f;
+        return buttonManage.TotalFullExecuteTime;
     }
 
 }
