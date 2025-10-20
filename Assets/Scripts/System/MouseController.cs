8ab6efd55ece6787f931af0527d9950c2e703214
yying8 <yueyan.ying0256@gmail.com>
2025-10-20 16:38:04 -0400

    save(在做鼠标ui savetime功能)

--- Assets/Scripts/System/MouseController.cs
+++ Assets/Scripts/System/MouseController.cs
@@ -7,11 +7,42 @@ public class MouseController : MonoBehaviour
     public Texture2D cursorTexture;
     public Vector2 hotspot = Vector2.zero;
     public CursorMode cursorMode = CursorMode.Auto;
+    public GameObject blankUI; //鼠标浮在save time的板上的白色框框
+    private Camera cam;
 
     void Start()
     {
+        //给savetimer board板块
+        cam = Camera.main;
+        blankUI.SetActive(false);
+
+        //鼠标皮肤
         Cursor.SetCursor(cursorTexture, hotspot, cursorMode);
         Cursor.lockState = CursorLockMode.None;
         Cursor.visible = true;
     }
+
+    void Update()
+    {
+        Vector2 mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+        RaycastHit2D hit = Physics2D.Raycast(mousePos, Vector2.zero);
+
+        if (hit.collider != null)
+        {
+            Debug.Log("Hit: " + hit.collider.name);
+
+            if (hit.collider.CompareTag("Selectable"))
+            {
+                blankUI.SetActive(true);
+                blankUI.transform.position = Input.mousePosition;
+            }
+            else blankUI.SetActive(false);
+        }
+        else
+        {
+            Debug.Log("没有射到任何物体");
+            blankUI.SetActive(false);
+        }
+    }
+
 }
